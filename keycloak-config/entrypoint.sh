95113fc6f2ccebf5f4a7a35692111125ba70f44a
arseni <r.seny.martysiuk@gmail.com>
2025-04-12 18:12:46 +0200

    fix entrypoint

--- keycloak-config/entrypoint.sh
+++ keycloak-config/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 if [ ! -f /opt/keycloak/data/import/.imported ]; then
-  /opt/keycloak/bin/kc.sh start-dev --import-realm
+  /opt/keycloak/bin/kc.sh start-dev --import-realm --features=scripts
   touch /opt/keycloak/data/import/.imported
 else
-  /opt/keycloak/bin/kc.sh start-dev
+  /opt/keycloak/bin/kc.sh start-dev --features=scripts
 fi
-
